5fd3fcea3f2c44058757fa38039cdccb97039fef
Krishna Gupta <krishna.gupta@rocketmail.com>
2018-09-09 23:40:22 +0530

    add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -15,6 +15,28 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	//socket.emit from admin text welcome to the chat app
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app'
+	});
+
+	//socket.broadcast.emit from admin text new user joined
+	socket.broadcast.emit('newMessage', {
+		from: "Admin",
+		text: "New user joined",
+		createdAt: new Date().getTime()
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log("Create Message:", message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
+	});
+	
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
 	});
